1995b1bccd110ef7f05e33010c75119514052d8a
MartinVoehringer <Martin.Voehringer@Student.Reutlingen-University.DE>
2015-03-11 14:43:51 +0100

    Konflikt

--- ErstesGit/src/HalloGit.java
+++ ErstesGit/src/HalloGit.java
@@ -4,7 +4,7 @@ public class HalloGit {
 	public static void main(String[] args) {
 		System.out.println("Hallo, ich bin ganz neuer Code...");
 		System.out.println("Hallo, bin auch drin :D");
-		System.out.println("Hallo du merger");
+		System.out.println("Hallo du merger, du");
 	}
 
 }

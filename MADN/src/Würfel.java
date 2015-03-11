e5cebcf41ceb2de0fbd86493ebe378f6903f8950
Max Schneller <Max.Schneller@student.reutlingen-univeristy.de>
2015-03-11 14:52:19 +0100

    Mit Kommentaren

--- MADN/src/Würfel.java
+++ MADN/src/Würfel.java
@@ -1,5 +1,10 @@
 
 public class Würfel {
+	
+	/*
+	 * @param random 
+	 * gibt Würfelzahl zwischen 1 und 6 zurück
+	 */
 	public static int wuerfeln(){
 		int random = (int) (6 * Math.random()) + 1;
 		System.out.println("Es wurde die Zahl: " + random + " gewürfelt!");
